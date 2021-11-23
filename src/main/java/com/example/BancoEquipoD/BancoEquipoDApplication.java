e853d91a80fe1438057dbe49e0134134c8724dcf
EdgarCampuzanoB <passwordgithub.1226>
2021-11-23 11:36:51 -0600

    Inicio del Proyecto

---
+++ src/main/java/com/example/BancoEquipoD/BancoEquipoDApplication.java
@@ -0,0 +1,13 @@
+package com.example.BancoEquipoD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoEquipoDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoEquipoDApplication.class, args);
+	}
+
+}
